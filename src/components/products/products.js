a30e2e614f12f7cb44d2fcd87e6a03321e1d6f77
ryan_tran <ryantran2165@gmail.com>
2020-10-24 19:41:09 -0700

    Remove products constructor

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -3,10 +3,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 class Products extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return <h1>PRODUCTS: {this.props.query}</h1>;
   }
