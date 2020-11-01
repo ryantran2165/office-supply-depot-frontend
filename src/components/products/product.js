fe19429f75f5f4f5cb8d869653b2ee90c61827cb
Thanh Tran <thanhngoctruc.tran@sjsu.edu>
2020-11-01 01:51:03 -0700

    Added buttons

--- src/components/products/product.js
+++ src/components/products/product.js
@@ -18,58 +18,63 @@ function Product() {
     console.log(product);
   }, [id]);
   
+ 
   // Make sure product is not null before rendering
   return (
     <Container fluid className="pb-5">
       {product ? (
         <div>
-          <Row className="pt-5 justtify-content-center ml-5">
+          <Row className="pt-5 justtify-content-center ml-5 pb-4 mb-4">
             <Col>
               <Image className="product-image" fluid src={product.img_url}/>
             </Col>
             <Col className="ml-3">
-              <h2>{product.name}</h2>
-              <p className="product-description mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
-                aute irure dolor in reprehenderit in voluptate velit esse 
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-                occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</p>
-              <h4 className="mt-3">${product.price}</h4>
+              <pre className="gray-text category-text-size">{product.category} | {product.subcategory}</pre>
+              <h3>{product.name}</h3>
+              <p className="product-description mt-3">{product.description}</p>
+              <h4 className="mt-3 mb-3">${product.price}</h4>
+              <span className="mt-3 product-detail">Weight&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{product.weight} lbs</span>
               <form className="mt-3" method="post">
                 <div>
                   <Row>
-                    <label className="qty-label pl-3 mr-3 mt-1">Quantity</label>
+                    <label className="product-detail pl-3 mr-3 mt-1">Quantity</label>
                     <Button className="button-round mr-2">-</Button>
                     <input className="quantity-input" type="text" value="1"/>
                     <Button className="button-round ml-2">+</Button>
-                    <Button className="add-to-cart-button ml-3" type="submit" value="Add to cart">Add to cart</Button>
+                    <Button className="add-to-cart-button ml-4" type="submit" value="Add to cart">Add to cart</Button>
                   </Row>
                 </div>
               </form>
             </Col>
             </Row>
-            <h4 className="ml-5 pl-3 mt-5">More from categories</h4>
-            <Row className="justify-content-center w-80 m-5">
-              <Col>
-              <Image className="product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
-              </Col>
-              <Col>
-              <Image className="product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
-              </Col>
-              <Col>
-              <Image className="product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
-              </Col>
-              <Col>
-              <Image className="product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
-              </Col>
-              <Col>
-              <Image className="product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
-              </Col>
+            <hr/>
+            <h4 className="text-center">More from categories</h4>
+            <hr/>
+            <p className="align mr-5">Page of </p>
+            <div className="d-flex flex-row align-items-center">
+              <div>
+                <Button className="m-3 align-self-start button-round">+</Button>
+              </div>
+              <div>
+              <Image className="p-3 product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
+              </div>
+              <div>
+                <Image className="p-3 product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
+              </div>
+              <div>
+                <Image className="p-3 product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
+              </div>
+              <div>
+                <Image className="p-3 product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
+              </div>
+              <div>
+                <Image className="p-3 product-image" fluid src="https://res.cloudinary.com/osd/image/upload/v1602180165/samples/ecommerce/accessories-bag.jpg"/>
+              </div>
+              <div>
+                <Button className="m-3 align-self-start button-round">+</Button>
+              </div>
+            </div>
             
-          </Row>
         </div>
        
       ): ""}
